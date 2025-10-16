6d2883ab176a0c973ff98615abb3cff558a5f3a1
MichaelKorchagin <benderrodriguez932@Gmail.com>
2025-10-15 19:16:50 -0700

    Created a new repo with a simple overridden DAO contract. Joined new HH, OZ. Set up all necessary configs.

---
+++ contracts/dao/ZDAO.sol
@@ -0,0 +1,201 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Governor } from "@openzeppelin/contracts/governance/Governor.sol";
+import { GovernorSettings } from "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import { GovernorCountingSimple } from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import { GovernorVotes, IVotes } from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import {
+    GovernorTimelockControl,
+    TimelockController
+} from "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import {
+    GovernorVotesQuorumFraction
+} from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import { GovernorPreventLateQuorum } from "@openzeppelin/contracts/governance/extensions/GovernorPreventLateQuorum.sol";
+
+
+contract ZDAO is
+    Governor,
+    GovernorSettings,
+    GovernorCountingSimple,
+    GovernorVotes,
+    GovernorTimelockControl,
+    GovernorVotesQuorumFraction,
+    GovernorPreventLateQuorum {
+
+    uint256 public daoId;
+
+    constructor(
+        uint256 _daoId,
+        string memory governorName,
+        IVotes token,
+        TimelockController timelock,
+        uint48 delay_,
+        uint32 votingPeriod_,
+        uint256 proposalThreshold_,
+        uint256 quorumPercentage_,
+        uint48 voteExtension_
+    )
+        Governor(governorName)
+        GovernorVotes(token)
+        GovernorTimelockControl(timelock)
+        GovernorSettings(
+            delay_,
+            votingPeriod_,
+            proposalThreshold_
+        )
+        GovernorVotesQuorumFraction(quorumPercentage_)
+        GovernorPreventLateQuorum(voteExtension_) {}
+
+    function votingDelay()
+    public
+    view
+    override(Governor, GovernorSettings)
+    returns (uint256) {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+    public
+    view
+    override(Governor, GovernorSettings)
+    returns (uint256) {
+        return super.votingPeriod();
+    }
+
+    function proposalThreshold()
+    public
+    view
+    override (Governor, GovernorSettings)
+    returns (uint256) {
+        return super.proposalThreshold();
+    }
+
+    function quorum(uint256 blockNumber)
+    public
+    view
+    override(Governor, GovernorVotesQuorumFraction)
+    returns (uint256) {
+        return super.quorum(blockNumber);
+    }
+
+    function state(uint256 proposalId)
+    public
+    view
+    override(Governor, GovernorTimelockControl)
+    returns (ProposalState) {
+        return super.state(proposalId);
+    }
+
+    function proposalNeedsQueuing(uint256 proposalId)
+    public
+    view
+    override(Governor, GovernorTimelockControl)
+    returns (bool) {
+        return super.proposalNeedsQueuing(proposalId);
+    }
+
+    function proposalDeadline(uint256 proposalId)
+    public
+    view
+    override(Governor, GovernorPreventLateQuorum)
+    returns (uint256) {
+        return super.proposalDeadline(proposalId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override
+    returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _queueOperations(
+        uint256 proposalId,
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    )
+    internal
+    override(Governor, GovernorTimelockControl)
+    returns (uint48) {
+        return super._queueOperations(
+            proposalId,
+            targets,
+            values,
+            calldatas,
+            descriptionHash
+        );
+    }
+
+    function _executeOperations(
+        uint256 proposalId,
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    )
+    internal
+    override(Governor, GovernorTimelockControl) {
+        super._executeOperations(
+            proposalId,
+            targets,
+            values,
+            calldatas,
+            descriptionHash
+        );
+    }
+
+    function _cancel(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    )
+    internal
+    override(Governor, GovernorTimelockControl)
+    returns (uint256) {
+        return super._cancel(
+            targets,
+            values,
+            calldatas,
+            descriptionHash
+        );
+    }
+
+    function _castVote(
+        uint256 proposalId,
+        address account,
+        uint8 support,
+        string memory reason,
+        bytes memory params
+    )
+    internal
+    override(Governor)
+    returns (uint256) {
+        return super._castVote(
+            proposalId,
+            account,
+            support,
+            reason,
+            params
+        );
+    }
+
+    function _tallyUpdated(uint256 proposalId)
+    internal
+    override(Governor, GovernorPreventLateQuorum) {
+        return super._tallyUpdated(proposalId);
+    }
+
+    function _executor()
+    internal
+    view
+    override(Governor, GovernorTimelockControl)
+    returns (address) {
+        return super._executor();
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.26",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 20000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+};
+
+export default config;
