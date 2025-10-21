194ee9fb307017848239912f6c255ac9c700c03e
MichaelKorchagin <benderrodriguez932@Gmail.com>
2025-10-20 19:16:08 -0700

    Test fixes. Added a common module with constants.

--- test/zDAO.test.ts
+++ test/zDAO.test.ts
@@ -1,36 +1,29 @@
 import hre from "hardhat";
 import { expect } from "chai";
 import { ethers } from "ethers";
-import { ZeroVotingERC20 } from "../types/ethers-contracts/voting/ZVoting20.sol/ZeroVotingERC20.js";
-import { ZDAO } from "../types/ethers-contracts/ZDAO.js";
+import { DEFAULT_ADMIN_ROLE } from "../src/constants.js";
+import { WalletClient } from "viem";
 
+let viem;
+let networkHelpers;
 
-let admin;
-let user1;
-let user2;
+let admin : WalletClient;
+let user1 : WalletClient;
+let user2 : WalletClient;
 
-let votingERC20 : ZeroVotingERC20;
-let governance20 : ZDAO;
-
-const delay = 1;
-const votingPeriod = 10;
+const delay = 1n;
+const votingPeriod = 10n;
 const proposalThreshold20 = ethers.parseUnits("100");
-const proposalThreshold721 = 1;
 const quorumPercentage = 10n;
-const voteExtension = 5;
+const voteExtension = 5n;
 
 describe("ZDAO", () => {
   const fixture = async () => {
-    const { viem, networkHelpers } = await hre.network.connect();
+    ({ viem, networkHelpers } = await hre.network.connect());
     [ admin, user1, user2 ] = await viem.getWalletClients();
   };
 
   it("should deploy Voting20 with DAO using viem", async () => {
-    const {
-      viem,
-      // networkHelpers
-    } = await hre.network.connect();
-
     // networkHelpers.loadFixture(fixture);
     await fixture();
 
@@ -39,98 +32,71 @@ describe("ZDAO", () => {
       "ZV",
       "ZERO DAO",
       "1",
-      admin.account.address
-    ]
+      admin.account?.address,
+    ];
 
+    const timelockParams = [
+      1n,
+      [],
+      [],
+      admin.account?.address,
+    ];
+
+    // Deploy VotingERC20
     const votingERC20 = await viem.deployContract(
       "ZeroVotingERC20",
-      [
-        "ZeroVotingERC20",
-        "ZV",
-        "ZERO DAO",
-        "1",
-        admin.account.address
-      ]
+      voting20Params
     );
-    expect(votingERC20).to.exist;
     expect(votingERC20.address).to.exist;
+    expect(await votingERC20.read.name()).to.equal("ZeroVotingERC20");
+    expect(await votingERC20.read.symbol()).to.equal("ZV");
 
-    // const timelock = await viem.deployContract(
-    //   "TimelockController",
-    //   [
-    //     1,
-    //     [],
-    //     [],
-    //     admin.account.address
-    //   ]
-    // );
-    // expect(timelock).to.exist;
+    // Deploy Timelock
+    const timelock = await viem.deployContract(
+      "TimelockController",
+      timelockParams
+    );
 
-  //   const governance20 = await viem.deployContract(
-  //     "ZDAO",
-  //     [
-  //       1n,
-  //       "ZDAO",
-  //       votingERC20.address,
-  //       timelock.address,
-  //       delay,
-  //       votingPeriod,
-  //       proposalThreshold20,
-  //       quorumPercentage,
-  //       voteExtension,
-  //     ]);
-  //   expect(governance20).to.exist;
+    expect(timelock).to.exist;
+    expect(await timelock.read.getMinDelay()).to.equal(timelockParams[0]);
+    expect(
+      await timelock.read.hasRole([
+        DEFAULT_ADMIN_ROLE,
+        admin.account.address,
+      ])
+    ).to.equal(true);
+
+    const govParams = [
+      1n,
+      "ZDAO",
+      votingERC20.address,
+      timelock.address,
+      delay,
+      votingPeriod,
+      proposalThreshold20,
+      quorumPercentage,
+      voteExtension,
+    ];
+
+    // Deploy Governance
+    const governance20 = await viem.deployContract(
+      "ZDAO",
+      govParams
+    );
+
+    expect(governance20).to.exist;
+    expect(await governance20.read.name()).to.equal(govParams[1]);
+    expect(
+      (await governance20.read.token()).toLowerCase()
+    ).to.equal(
+      govParams[2].toLowerCase()
+    );
+    expect(
+      (await governance20.read.timelock()).toLowerCase()
+    ).to.equal(
+      govParams[3].toLowerCase()
+    );
+    expect(await governance20.read.votingPeriod()).to.equal(govParams[5]);
+    expect(await governance20.read.proposalThreshold()).to.equal(govParams[6]);
   });
 });
-
-// const { viem } = await network.connect();
-// votingERC20 = await viem.deployContract(
-//   "ZeroVotingERC20",
-//   [
-//     "ZV",
-//     "domname",
-//     "ZERO DAO",
-//     "1",
-//     "0x1234567890123456789012345678901234567890"
-//   ]
-// );
-
-// describe("ZDAO", () => {
-//   let dao;
-
-//   it("load fixture", async () => {
-//     const { viem } = await network.connect();
-//     votingERC20 = await viem.deployContract("ZeroVotingERC20");
-
-//     expect(votingERC20).to.exist;
-//     expect(votingERC20).to.not.be.undefined;
-//   });
-
-
-//   // beforeEach(async function () {
-    
-//   //   const voting20 = await viem.deployContract("ZeroVotingERC20");
-//   //   // const voting20 = await viem.deployContract("ZeroVotingERC20");
-//   //   const token = await voting20.deploy(
-//   //     "ZeroVotingERC20",
-//   //     "ZV",
-//   //     "ZERO DAO",
-//   //     "1",
-//   //     (await ethers.getSigners())[0]
-//   //   );
-//   //   await token.waitForDeployment();
-
-//   //   const ZDAO = await ethers.getContractFactory("ZDAO");
-//   //   dao = await ZDAO.deploy(
-//   //     1n,
-//   //     "ZDAO",
-//   //     token,
-//   //     delay,
-//   //     votingPeriod,
-//   //     proposalThreshold20,
-//   //     quorumPercentage,
-//   //     voteExtension,
-//   //   );
-//   //   await dao.waitForDeployment();
-//   // });
-// });
